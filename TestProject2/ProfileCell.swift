580fc5b628126b7806cfc17133f0d53e8de706c6
KrishnaReddySycamores <vkamjula@sycamores.indstate.edu>
2018-03-04 00:36:55 -0500

    Files Added.

---
+++ TestProject2/ProfileCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCell.swift
+//  TestProject2
+//
+//  Created by vamsi krishna reddy kamjula on 3/4/18.
+//  Copyright © 2018 vamsi krishna reddy kamjula. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    
+}
